e178ea168f91a54a1e8af5db99a78aef9146971d
LeoSergio <leandrooos222@gmail.com>
2026-09-21 20:28:01 -0300

    Receita 6 - Q3: propriedades style e ibu nos objetos; tabela exibe 4 colunas

--- html_css_JS/receita6/script.js
+++ html_css_JS/receita6/script.js
@@ -1,23 +1,33 @@
 const cervejas = [
-    { name: "Guinness",      alcohol: "4.2%" },
-    { name: "Desperados",    alcohol: "5.9%" },
-    { name: "Becks",         alcohol: "5.0%" },
-    { name: "Heineken",      alcohol: "5.0%" },
-    { name: "Corona",        alcohol: "4.6%" },
-    { name: "Stella Artois", alcohol: "5.2%" },
+    { name: "Guinness",      alcohol: "4.2%", style: "Imperial Stout", ibu: "45" },
+    { name: "Desperados",    alcohol: "5.9%", style: "Lager",          ibu: "15" },
+    { name: "Becks",         alcohol: "5.0%", style: "Pilsen",         ibu: "20" },
+    { name: "Heineken",      alcohol: "5.0%", style: "Lager",          ibu: "23" },
+    { name: "Corona",        alcohol: "4.6%", style: "Cream Ale",      ibu: "18" },
+    { name: "Stella Artois", alcohol: "5.2%", style: "Pilsen",         ibu: "30" },
 ];
 
 const carregarDiv = (cervs) => {
     const div = document.getElementById("cervejasDiv");
 
     const rows = cervs.map(c =>
-        `<tr><td>${c.name}</td><td>${c.alcohol}</td></tr>`
+        `<tr>
+            <td>${c.name}</td>
+            <td>${c.alcohol}</td>
+            <td>${c.style}</td>
+            <td>${c.ibu}</td>
+        </tr>`
     ).join("\n");
 
     div.innerHTML = `
         <table>
             <thead>
-                <tr><th>Nome</th><th>Álcool</th></tr>
+                <tr>
+                    <th>Nome</th>
+                    <th>Álcool</th>
+                    <th>Estilo</th>
+                    <th>Amargor</th>
+                </tr>
             </thead>
             <tbody>${rows}</tbody>
         </table>
